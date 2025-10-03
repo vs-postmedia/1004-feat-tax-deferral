bfb41328f817ec6bd80645a3aad6e543e75b6cd3
Nathan Griffiths <ngriffiths@postmedia.com>
2025-10-02 17:05:45 -0700

    spell check

--- src/data/steps.js
+++ src/data/steps.js
@@ -1,7 +1,7 @@
 export default [
-    'The 25 most expensive B.C. properties that applied for property tax deferments have a combined value of over $460 million, according to B.C. Assessment’s 2025 valuations. <br><br> The deferment allows home owners aged 55 or older to delay paying property taxes until the home is sold – and the interest on the loan is very low. <span class="scroll-more">Scroll for more...</span>',
+    'The 25 most expensive B.C. properties that applied for property tax deferments have a combined value of over <span style="color:#0062A3">$460 million</span>, according to B.C. Assessment’s 2025 valuations. <br><br> The deferment allows home owners aged 55 or older to delay paying property taxes until the home is sold – and the interest on the loan is very low. <span class="scroll-more">Scroll for more...</span>',
     'Seven homes in Kitsilano with the tax deferment were assessed at $156.5 million and five in UBC and Point Grey were assessed at $82.6 million.',
-    'Seven in Shaunessey and South Vancouver were assessed at $121.4 million...',
+    'Seven in Shaughnessey and South Vancouver were assessed at $121.4 million...',
     'and another five in West Vancouver were assessed at $67.9 million.',
     'The high value of some of the homes receiving tax deferments have helped contribute to the total value of outstanding loans more than tripling since 2014.'
 ];
